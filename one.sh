e6bc2f27b382ade9586aa94b6e935b4119ccb990
Robin Bron <robin@finwo.nl>
2018-11-29 14:20:39 +0100

    More stability

--- one.sh
+++ one.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
-WD="$(dirname $0)"
 
-JSE=$(command -v node mujs | head -n 1)
+# Get env
+WD="$(dirname $0)"
+JSE=$(command -v node nodejs mujs | head -n 1)
+
+# Update submodules
 git submodule update --init --recursive &>/dev/null
-FILE=$(find "${WD}/jar" -type f -name *.json | shuf -n 1)
-cat "${FILE}" | $JSE $WD/one.js
+
+# Find file & print it
+FILE=$(find "${WD}/jar" -type f -name \*.json | shuf -n 1)
+[ -z "${FILE}" ] || cat "${FILE}" | $JSE $WD/one.js
