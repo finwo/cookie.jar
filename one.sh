bc963c71b1c6ef374aed1b91e66cc6fa97588271
Robin Bron <robin@finwo.nl>
2018-11-29 14:15:56 +0100

    Ensure single args

--- one.sh
+++ one.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-WD=$(dirname $0)
+WD="$(dirname $0)"
 
 JSE=$(command -v node mujs | head -n 1)
 git submodule update --init --recursive &>/dev/null
-FILE=$(find $WD/jar -type f -name *.json | shuf -n 1)
+FILE=$(find "${WD}/jar" -type f -name *.json | shuf -n 1)
 cat "${FILE}" | $JSE $WD/one.js
